7c0581e348fd7373b07897ab9915cb3b3da7c43e
Alejandro Mildiner <alejandro.mildiner@gmail.com>
2020-08-06 01:41:48 -0300

    adding webapp folder as part of the classpath

--- src/main/java/ar/com/eureka/notification/email/configurations/MailConfiguration.java
+++ src/main/java/ar/com/eureka/notification/email/configurations/MailConfiguration.java
@@ -27,7 +27,7 @@ public class MailConfiguration {
   @Bean
   public SpringResourceTemplateResolver thymeleafTemplateResolver() {
     SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-    templateResolver.setPrefix("templates/mails/");
+    templateResolver.setPrefix("/WEB-INF/mails/");
     templateResolver.setSuffix(".html");
     templateResolver.setTemplateMode("HTML");
     templateResolver.setCharacterEncoding("UTF-8");
